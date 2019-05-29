7d3d2a1c3428c29d0be8b91a304f2592e61bece8
liuyandong <554030404@qq.com>
2019-05-29 18:51:05 +0800

    错误信息格式调整！！

--- src/main/java/build/dream/api/configurations/SyncDataConfiguration.java
+++ src/main/java/build/dream/api/configurations/SyncDataConfiguration.java
@@ -1,10 +1,10 @@
 package build.dream.api.configurations;
 
 import build.dream.api.domains.BaseDomain;
-import build.dream.api.domains.TmpFdb;
+import build.dream.api.domains.TmpSpb;
 
 public class SyncDataConfiguration {
-    public static final Class<? extends BaseDomain>[] SYNC_DOMAIN_CLASSES = new Class[]{TmpFdb.class};
+    public static final Class<? extends BaseDomain>[] SYNC_DOMAIN_CLASSES = new Class[]{TmpSpb.class};
     public static final String KEY_SYNC_DATA = "_sync_data";
     public static final String FIELD_NAME_DOMAIN_CLASS_NAME = "domainClassName";
     public static final String FIELD_NAME_DATA = "data";
---
+++ src/main/java/build/dream/api/domains/TmpSpb.java
@@ -0,0 +1,195 @@
+package build.dream.api.domains;
+
+import java.math.BigDecimal;
+
+public class TmpSpb extends BaseDomain {
+    private String identityInfo;
+    private String sfdbh;
+    private String sspbh;
+    private String sspmc;
+    private String sdw;
+    private String sfl;
+    private String sgys;
+    private BigDecimal njj;
+    private BigDecimal nsj;
+    private BigDecimal nzdsl;
+    private BigDecimal nzgsl;
+    private BigDecimal nzdcl;
+    private BigDecimal ncgbzs;
+    private BigDecimal nbzt;
+    private String ssfkt;
+    private String sspbhbz;
+    private BigDecimal npms;
+    private String ssfkj;
+    private String ssfks;
+    private String syt;
+    private String szdbh;
+
+    public String getIdentityInfo() {
+        return identityInfo;
+    }
+
+    public void setIdentityInfo(String identityInfo) {
+        this.identityInfo = identityInfo;
+    }
+
+    public String getSfdbh() {
+        return sfdbh;
+    }
+
+    public void setSfdbh(String sfdbh) {
+        this.sfdbh = sfdbh;
+    }
+
+    public String getSspbh() {
+        return sspbh;
+    }
+
+    public void setSspbh(String sspbh) {
+        this.sspbh = sspbh;
+    }
+
+    public String getSspmc() {
+        return sspmc;
+    }
+
+    public void setSspmc(String sspmc) {
+        this.sspmc = sspmc;
+    }
+
+    public String getSdw() {
+        return sdw;
+    }
+
+    public void setSdw(String sdw) {
+        this.sdw = sdw;
+    }
+
+    public String getSfl() {
+        return sfl;
+    }
+
+    public void setSfl(String sfl) {
+        this.sfl = sfl;
+    }
+
+    public String getSgys() {
+        return sgys;
+    }
+
+    public void setSgys(String sgys) {
+        this.sgys = sgys;
+    }
+
+    public BigDecimal getNjj() {
+        return njj;
+    }
+
+    public void setNjj(BigDecimal njj) {
+        this.njj = njj;
+    }
+
+    public BigDecimal getNsj() {
+        return nsj;
+    }
+
+    public void setNsj(BigDecimal nsj) {
+        this.nsj = nsj;
+    }
+
+    public BigDecimal getNzdsl() {
+        return nzdsl;
+    }
+
+    public void setNzdsl(BigDecimal nzdsl) {
+        this.nzdsl = nzdsl;
+    }
+
+    public BigDecimal getNzgsl() {
+        return nzgsl;
+    }
+
+    public void setNzgsl(BigDecimal nzgsl) {
+        this.nzgsl = nzgsl;
+    }
+
+    public BigDecimal getNzdcl() {
+        return nzdcl;
+    }
+
+    public void setNzdcl(BigDecimal nzdcl) {
+        this.nzdcl = nzdcl;
+    }
+
+    public BigDecimal getNcgbzs() {
+        return ncgbzs;
+    }
+
+    public void setNcgbzs(BigDecimal ncgbzs) {
+        this.ncgbzs = ncgbzs;
+    }
+
+    public BigDecimal getNbzt() {
+        return nbzt;
+    }
+
+    public void setNbzt(BigDecimal nbzt) {
+        this.nbzt = nbzt;
+    }
+
+    public String getSsfkt() {
+        return ssfkt;
+    }
+
+    public void setSsfkt(String ssfkt) {
+        this.ssfkt = ssfkt;
+    }
+
+    public String getSspbhbz() {
+        return sspbhbz;
+    }
+
+    public void setSspbhbz(String sspbhbz) {
+        this.sspbhbz = sspbhbz;
+    }
+
+    public BigDecimal getNpms() {
+        return npms;
+    }
+
+    public void setNpms(BigDecimal npms) {
+        this.npms = npms;
+    }
+
+    public String getSsfkj() {
+        return ssfkj;
+    }
+
+    public void setSsfkj(String ssfkj) {
+        this.ssfkj = ssfkj;
+    }
+
+    public String getSsfks() {
+        return ssfks;
+    }
+
+    public void setSsfks(String ssfks) {
+        this.ssfks = ssfks;
+    }
+
+    public String getSyt() {
+        return syt;
+    }
+
+    public void setSyt(String syt) {
+        this.syt = syt;
+    }
+
+    public String getSzdbh() {
+        return szdbh;
+    }
+
+    public void setSzdbh(String szdbh) {
+        this.szdbh = szdbh;
+    }
+}
