9ce78af20bfc3718608d8a14a975fce080c0d4ec
liuyandong <554030404@qq.com>
2020-01-19 20:28:51 +0800

    初始化项目！！

--- src/main/java/build/dream/api/controllers/SSOController.java
+++ src/main/java/build/dream/api/controllers/SSOController.java
@@ -1,9 +1,11 @@
 package build.dream.api.controllers;
 
+import build.dream.api.constants.Constants;
 import build.dream.common.utils.ApplicationHandler;
 import build.dream.common.utils.UrlUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +15,13 @@ import java.util.Map;
 @Controller
 @RequestMapping(value = "/sso")
 public class SSOController {
+    /**
+     * 重定向单点登录
+     *
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @return
+     */
     @RequestMapping(value = "/login")
     public String login(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters(httpServletRequest);
@@ -23,5 +32,21 @@ public class SSOController {
 
         httpServletResponse.addCookie(new Cookie("ACCESS_TOKEN", accessToken));
         return "redirect:" + redirectUrl + "?token=" + accessToken + "&index=" + index + "&redirectUrl=" + UrlUtils.encode(originalRedirectUrl);
+    }
+
+    /**
+     * ajax 单点登录
+     *
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @return
+     */
+    @RequestMapping(value = "/ajaxLogin")
+    @ResponseBody
+    public String ajaxLogin(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        Map<String, String> requestParameters = ApplicationHandler.getRequestParameters(httpServletRequest);
+        String accessToken = requestParameters.get("accessToken");
+        httpServletResponse.addCookie(new Cookie("ACCESS_TOKEN", accessToken));
+        return Constants.SUCCESS;
     }
 }
